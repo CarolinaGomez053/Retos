366e3930c1765d78e47ba7a0f576b0317fd80572
LEIDY CAROLINA GÓMEZ SÁNCHEZ - ANALISTA DE CALIDAD <lgomez@amym.loc>
2021-03-22 15:00:02 -0500

    Reto Calcular Distancia
    Retos calcular distancia en 1 sola linea

---
+++ RetoCalcularDistancia.js
@@ -0,0 +1,6 @@
+//Reto calcular distancia en 1 linea de codigo
+
+let calcularDistancia=(xinicial,xfinal,yinicial,yfinal)=>{return Math.sqrt(Math.pow((xfinal-xinicial),2) + Math.pow((yfinal-yinicial),2));}
+
+console.log(calcularDistancia(0,70,0,50));
+
